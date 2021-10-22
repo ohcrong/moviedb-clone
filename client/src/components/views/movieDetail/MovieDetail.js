2843e4516ba1fed26cb5a0af1871a275228e98f8
kyuyoungoh <oqoq59@gmail.com>
2021-10-22 21:18:39 +0900

    movie detail page

---
+++ client/src/components/views/movieDetail/MovieDetail.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config'
+import MainImage from '../LandingPage/Sections/MainImage'
+import MovieInfo from './Sections/MovieInfo'
+
+function MovieDetail(props) {
+
+    const [Movie, setMovie] = useState([])
+
+    let movieId = props.match.params.movieId
+
+    useEffect(() => {
+        //console.log(props.match)
+        let endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`
+        let endpointInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}`
+
+        fetch(endpointInfo)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+                setMovie(response)
+            })
+    }, [])
+
+    return (
+        <div>
+            {/*Header */}
+            <MainImage 
+                image={`${IMAGE_BASE_URL}w1280${Movie.backdrop_path}`}
+                title={Movie.original_title}
+                text={Movie.overview}
+            />
+            <div>
+                {/*Body */}
+                <div style={{width:'85%', margin:'1rem auto'}}>
+                    
+                    {/*Movie Info */}
+                    <MovieInfo 
+                        movie ={Movie}
+                    />
+                    
+                    <br/>
+
+                    {/*Actors Grid */}
+                    <div style={{display:'flex', justifyContent:'center', margin:'2rem'}}>
+                        
+                    </div>
+
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default MovieDetail
