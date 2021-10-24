ab9ec3c5e788c315c71395ede9290ba83d7bb807
kyuyoungoh <oqoq59@gmail.com>
2021-10-24 13:53:22 +0900

    cast info grids

--- client/src/components/views/movieDetail/MovieDetail.js
+++ client/src/components/views/movieDetail/MovieDetail.js
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config'
 import MainImage from '../LandingPage/Sections/MainImage'
 import MovieInfo from './Sections/MovieInfo'
+import GridCards from '../commons/GridCards'
+import { Row } from 'antd'
+
 
 function MovieDetail(props) {
 
     const [Movie, setMovie] = useState([])
+    const [Casts, setCasts] = useState([])
+    const [ActorToggle, setActorToggle] = useState(false)
 
     let movieId = props.match.params.movieId
 
@@ -17,10 +22,21 @@ function MovieDetail(props) {
         fetch(endpointInfo)
             .then(response => response.json())
             .then(response => {
-                console.log(response)
+                console.log('responseMovieInfo',response)
                 setMovie(response)
             })
+        
+        fetch(endpointCrew)
+            .then(response => response.json())
+            .then(response => {
+                console.log('responseCast',response)
+                setCasts(response.cast)
+            })    
     }, [])
+
+    const loadMoreActors = ()=> {
+        setActorToggle(!ActorToggle)
+    }
 
     return (
         <div>
@@ -44,9 +60,24 @@ function MovieDetail(props) {
                     <br/>
 
                     {/*Actors Grid */}
-                    <div style={{display:'flex', justifyContent:'center', margin:'2rem'}}>
+                    <div style={{display:'flex', justifyContent:'center', margin:'2rem'}} >
+                        <button onClick={loadMoreActors}>View Actors</button>
+                    </div>    
+
+                    {ActorToggle && 
+                        <Row gutter={[16,16]}>
+                            {Casts&& Casts.map((cast, index)=> (
+                                <React.Fragment key={index}>
+                                    <GridCards
+                                        image={cast.profile_path ?
+                                            `${IMAGE_BASE_URL}w500${cast.profile_path}`: null}
+                                        actorName={cast.name} />
+                                </React.Fragment>    
+                            ))}
+                        </Row>
+                    }
                         
-                    </div>
+                    
 
                 </div>
             </div>
